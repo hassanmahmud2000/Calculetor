770c51be4aacc3f538ba77d8e1db8c6e30f1bda8
Mahmudul Hassan <hassanmahmud5556@gmail.com>
2023-08-18 01:16:05 +0600

    first commit

--- JS/TrinagleAREA.js
+++ JS/TrinagleAREA.js
@@ -18,6 +18,24 @@ function triangleArea () {
     const AreaField = document.getElementById ('areaField');
     AreaField.innerText = area;
 
-    
+}
+//rectangle  Area  !!!!!!
 
+function rectangleArea  () {
+    // Get Width Value !!!!!!!
+    const rectangleWidthField = document.getElementById ('rectangle-width');
+    const rectangleWidthFieldString = rectangleWidthField.value;
+    const rectangleWidth = parseFloat (rectangleWidthFieldString);
+
+    // Get Length Value  !!!!!!
+    const rectangleLengthField = document.getElementById ('rectangle-length');
+    const rectangleLengthFieldString = rectangleLengthField.value;
+    const rectangleLength = parseFloat (rectangleLengthFieldString);
+    // Area Calculetor  !!!!!!
+
+    const areaRectangler = rectangleWidth * rectangleLength;
+
+    const areaCalculetField = document.getElementById ('rectangleAreaField');
+    areaCalculetField.innerText = areaRectangler;
+    
 }
